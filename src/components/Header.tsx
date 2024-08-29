3b38da5aa0b90e4865c427e3d703d5afaef43387
mauriani <maurianim@gmail.com>
2024-08-29 16:56:07 -0300

    adicionando font no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export function Header({ title }: IProps) {
     <View
       className={'bg-secondary h-32 items-center - justify-center'}
       style={styles.header}>
-      <Text className="text-white text-center font-bold text-md mb-6">
+      <Text className="text-white text-center primary_bold font-bold text-md mb-6">
         {title}
       </Text>
     </View>
