fefcbf5f6aa3786e39b654e7f99cab90ec4fe01c
mauriani <maurianim@gmail.com>
2024-10-10 16:48:37 -0300

    adicionando correcao em card history

--- src/screens/History/__components__/MyTrainingHistoryCard.tsx
+++ src/screens/History/__components__/MyTrainingHistoryCard.tsx
@@ -10,7 +10,7 @@ type IProps = {
 
 export function MyTrainingHistoryCard({ item }: IProps) {
   return (
-    <View className="h-28 flex-row gap-4 bg-secondary rounded-[8px] items-center p-2 relative">
+    <View className="min-h-32 flex-row gap-4 bg-secondary rounded-[8px] items-center p-2 relative">
       <Image
         className="h-full w-20 rounded-[6px]"
         source={{
@@ -19,7 +19,7 @@ export function MyTrainingHistoryCard({ item }: IProps) {
         alt=""
       />
 
-      <View className="flex-col justify-center gap-[2] ml-3 max-w-40">
+      <View className="flex-col justify-center gap-1 ml-3">
         <Text
           className="text-white font-primary_bold text-base"
           numberOfLines={1}
@@ -30,7 +30,7 @@ export function MyTrainingHistoryCard({ item }: IProps) {
           {item.sets} séries x {item.reps} repetições
         </Text>
 
-        <View className="flex-row gap-2">
+        <View className="flex-row gap-2 mt-2">
           <ContentDetails
             value={`${formatTime(item.restTime)}min`}
             label={'Tempo de Descanso'}
